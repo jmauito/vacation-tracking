63e31ca023baba172aed9cb3492226d04942dfb1
erika salome <kikaastudillo2005@gmail.com>
2023-03-06 11:12:57 -0500

    Eleftsidebar

---
+++ frontend/vacation-tracking/src/components/template/LeftSidebar.jsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import Divider from '@mui/material/Divider';
+
+const style = {
+    width: '150%',
+    maxWidth: 500,
+    bgcolor: 'darkgrey',
+    
+    
+  };
+
+export const LeftSidebar = () => {
+  return (
+    <List sx={style} component="nav" aria-label="mailbox folders">
+        <ListItem button>
+        <ListItemText primary="Gestor de vacaciones" />
+        </ListItem>
+        <Divider/>
+        <ListItem button divider>
+            <ListItemText primary="Inicio"/>
+        </ListItem>
+        <ListItem button>
+            <ListItemText primary="Solicitar vacaciones"/>
+        </ListItem>
+        <Divider light/>
+        <ListItem button>
+            <ListItemText primary="Estado de las solicitudes"/>
+        </ListItem>
+    </List>
+  );
+}
--- frontend/vacation-tracking/src/components/template/MainTemplate.jsx
+++ frontend/vacation-tracking/src/components/template/MainTemplate.jsx
@@ -3,6 +3,9 @@ import { styled } from '@mui/material/styles';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
 import { TopSidebar } from './TopSidebar';
+import { LeftSidebar } from './LeftSidebar';
+
+
 
 const Item = styled(Paper)(({ theme }) => ({
   backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
@@ -18,12 +21,13 @@ export const MainTemplate = () => {
     <Grid container spacing={2}>
       <Grid
         item xs={12}>
-           <TopSidebar/> 
+        <TopSidebar />
       </Grid>
 
       <Grid item xs={3}>
-        <Item>xs=8</Item>
+        <LeftSidebar />
       </Grid>
+
       <Grid item xs={9}>
         <Item>xs=4</Item>
       </Grid>
