f2c91a5b268fb9c9b279958ca77a9d2746b1e976
mabel <mabelpucha04@gmail.com>
2023-03-07 14:19:20 -0500

    se crea pagina  de datos

--- frontend/vacation-tracking/src/components/VacationRequestList.jsx
+++ frontend/vacation-tracking/src/components/VacationRequestList.jsx
@@ -1,52 +1,66 @@
 import React from 'react'
-import { Box } from '@mui/material';
+import { Box, Grid } from '@mui/material';
 import { DataGrid } from '@mui/x-data-grid';
+import Button from '@mui/material/Button';
+
 const columns = [
-  { field: 'id', headerName: 'ID', width: 90 },
+  { field: 'id', headerName: 'Nro', width: 90 },
   {
-    field: 'lastName',
-    headerName: 'last name',
+    field: 'Nombres',
+    headerName: 'Nombres y Apellidos',
     width: 150,
     editable: true,
   },
   {
-    field: 'firstName',
-    headerName: 'first name',
+    field: 'Tipo',
+    headerName: 'Tipo',
+    width: 250,
+    editable: true,
+  },
+  {
+    field: 'Inicia',
+    headerName: 'Inicia',
     width: 150,
     editable: true,
   },
   {
-    field: 'age',
-    headerName: 'Age',
-    type: 'number',
+    field: 'Termina',
+    headerName: 'Termina',
+    width: 150,
+    editable: true,
+  },
+  {
+    field: 'Accion',
+    headerName: 'Accion',
     width: 110,
     editable: true,
   },
-    
 ];
 
 const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', age: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
+  { id: 1, Nombres: 'Snow', Tipo: 'Tiempo de vacaciones', Inicia: '01-Ene-2023', Termina:'15-Ene-2023', Accion:'Validar'},
+  { id: 2, Nombres: 'Lannister', Tipo: 'Licencia', Inicia: '01-Ene-2023', Termina:'15-Ene-2023', Accion:'Validar'},
+  { id: 3, Nombres: 'Lannister', Tipo: 'Tiempo de vacaciones', Inicia: '30-Ene-2023', Termina:'15-Feb-2023', Accion:'Validar'},
+  { id: 4, Nombres: 'Stark', Tipo: 'Licencia', Inicia: '20-Feb-2023', Termina: '01-Mar-2023', Accion:'Validar'},
+  
 ];
 
 export const VacationRequestList = () => {
   return (
     <div>
       <h1>Lista de solicitudes de vacaciones</h1>
-      <Box sx={{ height: 400, width: '100%' }}>
+      <Box sx={{ height:315 , width: '100%' }}>
         <DataGrid
           rows={rows}
           columns={columns}
         />
       </Box>
+
+      <Grid container spacing={0} > 
+      <Button variant="contained">Registar el elemento </Button>
+      </Grid>  
+
+
     </div>
   )
 }
