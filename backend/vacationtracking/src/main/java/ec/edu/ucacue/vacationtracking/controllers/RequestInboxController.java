91fdc8539e7b05e1d6a5f616ec22f9f4626b8341
Mauricio Zárate <mzaratep@ucacue.edu.ec>
2023-03-07 11:06:29 -0500

    Persistence layer to RequestDAO was implemented

--- backend/vacationtracking/src/main/java/ec/edu/ucacue/vacationtracking/controllers/RequestInboxController.java
+++ backend/vacationtracking/src/main/java/ec/edu/ucacue/vacationtracking/controllers/RequestInboxController.java
@@ -1,6 +1,8 @@
 package ec.edu.ucacue.vacationtracking.controllers;
 
 import ec.edu.ucacue.vacationtracking.domain.Request;
+import ec.edu.ucacue.vacationtracking.services.RequestService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,15 +13,18 @@ import java.util.List;
 @Controller
 @RequestMapping("/request-inbox")
 public class RequestInboxController {
+    @Autowired
+    RequestService requestService;
     @GetMapping
     public ResponseEntity<List<Request>> findAll(){
-        List<Request> requests = List.of(
+        /*List<Request> requests = List.of(
             Request.builder().title("Solicitud uno").comment("Comentario 1").hours(0).dates( List.of("2023-01-01", "2023-01-15") ).build(),
             Request.builder().title("Solicitud dos").comment("Comentario 2").hours(0).dates( List.of("2023-02-01", "2023-02-20") ).build(),
             Request.builder().title("Solicitud tres").comment("Comentario 3").hours(0).dates( List.of("2023-02-01", "2023-02-20") ).build(),
             Request.builder().title("Solicitud cuatro").comment("Comentario 4").hours(0).dates( List.of("2023-02-01", "2023-02-20") ).build(),
             Request.builder().title("Solicitud cinco").comment("Comentario 5").hours(0).dates( List.of("2023-02-01", "2023-02-20") ).build()
-        );
+        );*/
+        List<Request> requests = requestService.findAll();
         return ResponseEntity.ok(requests);
     }
 }
--- backend/vacationtracking/src/main/java/ec/edu/ucacue/vacationtracking/domain/Request.java
+++ backend/vacationtracking/src/main/java/ec/edu/ucacue/vacationtracking/domain/Request.java
@@ -1,16 +1,27 @@
 package ec.edu.ucacue.vacationtracking.domain;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.Date;
 import java.util.List;
 @Builder
 @Data
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
 public class Request {
+    @Id
+    @GeneratedValue
+    private Long id;
     private String title;
     private String comment;
     private int hours;
-    private List<String> dates;
+    private Date startDate;
+    private Date finishDate;
 }
