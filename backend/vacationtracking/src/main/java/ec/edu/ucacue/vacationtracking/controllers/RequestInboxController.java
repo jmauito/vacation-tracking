073f93f6487adb9bf2e02bd524f18d1748e577ea
Mauricio Zárate <mzaratep@ucacue.edu.ec>
2023-03-07 11:11:58 -0500

    comments from pull request are attented

--- backend/vacationtracking/src/main/java/ec/edu/ucacue/vacationtracking/controllers/RequestInboxController.java
+++ backend/vacationtracking/src/main/java/ec/edu/ucacue/vacationtracking/controllers/RequestInboxController.java
@@ -17,13 +17,6 @@ public class RequestInboxController {
     RequestService requestService;
     @GetMapping
     public ResponseEntity<List<Request>> findAll(){
-        /*List<Request> requests = List.of(
-            Request.builder().title("Solicitud uno").comment("Comentario 1").hours(0).dates( List.of("2023-01-01", "2023-01-15") ).build(),
-            Request.builder().title("Solicitud dos").comment("Comentario 2").hours(0).dates( List.of("2023-02-01", "2023-02-20") ).build(),
-            Request.builder().title("Solicitud tres").comment("Comentario 3").hours(0).dates( List.of("2023-02-01", "2023-02-20") ).build(),
-            Request.builder().title("Solicitud cuatro").comment("Comentario 4").hours(0).dates( List.of("2023-02-01", "2023-02-20") ).build(),
-            Request.builder().title("Solicitud cinco").comment("Comentario 5").hours(0).dates( List.of("2023-02-01", "2023-02-20") ).build()
-        );*/
         List<Request> requests = requestService.findAll();
         return ResponseEntity.ok(requests);
     }
--- backend/vacationtracking/src/main/java/ec/edu/ucacue/vacationtracking/domain/Request.java
+++ backend/vacationtracking/src/main/java/ec/edu/ucacue/vacationtracking/domain/Request.java
@@ -9,7 +9,6 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
-import java.util.List;
 @Builder
 @Data
 @Entity
