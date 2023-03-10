7909387139e92c6b01ed1cdd0342fa2be18ede9e
jmauito <jmauito@gmail.com>
2023-03-09 21:52:07 -0500

    Implemented connection whit postgesql database

--- backend/vacationtracking/src/main/resources/data.sql
+++ backend/vacationtracking/src/main/resources/data.sql
@@ -2,14 +2,14 @@ INSERT INTO ROLE(ID, NAME)
 VALUES (1, 'Gestor de vacaciones'),
 (2, 'Programador');
 --
---INSERT INTO _USER(ID, PASSWORD, EMAIL, ROLE_ID)
---VALUES(1, 'pass123', 'mauricio.zarate', 2),
---(2, 'pass123', 'juana.ramirez', 1);
+INSERT INTO _USER(ID, PASSWORD, EMAIL, ROLE_ID)
+VALUES(100, '$2a$10$iOp5PLISgwL.xxdvQFl14uT506s8bYXR4B./3QU6teIkFRno3bUjq', 'nick.fury@mail.com', 1),
+(200, '$2a$10$629nhy94B86rW16/KdJOoet.f3XnAPC8CmKxyLjWPXt8eXHwFvaui', 'natasha.romanoff@mail.com', 2);
 --
 INSERT INTO REQUEST(ID, TITLE, COMMENT, FINISH_DATE, START_DATE, HOURS)
 VALUES
-(1, 'Solicitud uno', 'Comentario 1', '2023-01-01', '2023-01-15',0),
-(2, 'Solicitud dos', 'Comentario 2', '2023-02-01', '2023-02-20',0),
-(3, 'Solicitud tres', 'Comentario 3', '2023-02-01', '2023-02-20',0),
-(4, 'Solicitud cuatro', 'Comentario 4', '2023-02-01', '2023-02-20',0),
-(5, 'Solicitud cinco', 'Comentario 5', '2023-02-01', '2023-02-20',0);
+(100, 'Solicitud uno', 'Comentario 1', '2023-01-01', '2023-01-15',0),
+(101, 'Solicitud dos', 'Comentario 2', '2023-02-01', '2023-02-20',0),
+(102, 'Solicitud tres', 'Comentario 3', '2023-02-01', '2023-02-20',0),
+(103, 'Solicitud cuatro', 'Comentario 4', '2023-02-01', '2023-02-20',0),
+(104, 'Solicitud cinco', 'Comentario 5', '2023-02-01', '2023-02-20',0);
