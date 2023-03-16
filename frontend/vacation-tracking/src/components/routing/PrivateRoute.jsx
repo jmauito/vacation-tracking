fa7ed5d1f3e74667987302cd3b24a5349d119a8f
William Suáres <william.suares@ucacue.edu.ec>
2023-03-16 08:49:46 -0500

    Se valida redireccionamiento de intercez por rol de usuario.

--- frontend/vacation-tracking/src/components/routing/PrivateRoute.jsx
+++ frontend/vacation-tracking/src/components/routing/PrivateRoute.jsx
@@ -1,17 +1,37 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom';
 import { StatusRequest } from '../../vacationTrackingProcess/statusRequest/StatusRequest';
 import { VacationRequest } from '../../vacationTrackingProcess/vacationRequest/VacationRequest';
 import { VacationRequestList } from '../../vacationTrackingProcess/vacationRequestList/VacationRequestList';
-// import { MainTemplate } from '../template/MainTemplate';
+import { UserContext } from '../contextManager/UserContext';
 
 export const PrivateRoute = () => {
+
+  const {statusLogin} = useContext(UserContext);
+  const {role} = statusLogin;
+
+  const redirectRoles = () => {
+    if(role.name === 'ROLE_USER'){
+      return (
+        <>
+          <Route path="/*" element={<Navigate to="/solicitud-vacaciones"/>}/>
+          <Route path="/solicitud-vacaciones" element={<VacationRequest />} exact />
+          <Route path="/estado-solicitudes-vacaciones" element={<StatusRequest />} exact />
+        </>
+      )
+    } else if(role.name === 'ROLE_ADMIN'){
+      return (
+        <>
+          <Route path="/*" element={<Navigate to="/revision-solicitudes-vacaciones"/>}/>
+          <Route path="/revision-solicitudes-vacaciones" element={<VacationRequestList />} exact />
+        </>
+      )
+    }
+  }
+
   return (
     <Routes>
-      <Route path="/solicitud-vacaciones" element={<VacationRequest />} exact />
-      <Route path="/estado-solicitudes-vacaciones" element={<StatusRequest />} exact />
-      <Route path="/revision-solicitudes-vacaciones" element={<VacationRequestList />} exact />
-      <Route path="/*" element={<Navigate to="/solicitud-vacaciones"/>}/>
+      { redirectRoles()}
     </Routes>
   )
 }
